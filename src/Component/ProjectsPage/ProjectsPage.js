40b7e78b00b8b016f57c487ff7cecbdead5d2b2c
“Batool <Rose.sy2001.tu@gmail.com>
2024-08-14 21:37:58 +0400

    add cooking

--- src/Component/ProjectsPage/ProjectsPage.js
+++ src/Component/ProjectsPage/ProjectsPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./projects.css";
 import { motion } from "framer-motion";
 import Khotta3 from "../../assets/images/Khotta3.JPG";
-import Admin9 from "../../assets/images/Admin9.png";
+import Cooking3 from "../../assets/images/Cooking3.png";
 import Consumer3 from "../../assets/images/Consumer3.png";
 import TenderHand from "../../assets/images/TenderHand.jpg";
 import Project2 from "../../assets/images/Project2.jpg";
@@ -33,39 +33,6 @@ const ProjectsPage = () => {
                 <div class="blog-item padd-15">
                   <div class="blog-item-inner shadow-dark">
                     <div class="blog-img">
-                      <img src={Khotta3} />
-                      <div class="blog-data">4 Feb 2021</div>
-                    </div>
-                    <div class="blog-info">
-                      <h4 class="blog-title">Khotta App | Axenda</h4>
-                      <p class="blog-descrip">Web Designer</p>
-                      <p class="blog-tags">
-                        Tags : <a href="#">Figma </a>, <a href=""> Jira </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="blog-item padd-15">
-                  <div class="blog-item-inner shadow-dark">
-                    <div class="blog-img">
-                      <img src={Admin9} />
-                      <div class="blog-data">1 Aug 2022</div>
-                    </div>
-                    <div class="blog-info">
-                      <h4 class="blog-title">Admin Portal | Lendo Financing</h4>
-                      <p class="blog-descrip">Web Development</p>
-                      <p class="blog-tags">
-                        Tags : <a href="#">React JS </a>,
-                        <a href=""> Bootstrap css </a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="blog-item padd-15">
-                  <div class="blog-item-inner shadow-dark">
-                    <div class="blog-img">
                       <img src={Consumer3} />
                       <div class="blog-data">1 Aug 2022</div>
                     </div>
@@ -76,7 +43,7 @@ const ProjectsPage = () => {
                       <p class="blog-descrip">Web Development</p>
                       <p class="blog-tags">
                         Tags : <a href="#">React JS </a>,
-                        <a href=""> Bootstrap css </a>
+                        <a href=""> Bootstrap Css </a>
                       </p>
                     </div>
                   </div>
@@ -84,8 +51,42 @@ const ProjectsPage = () => {
                 <div class="blog-item padd-15">
                   <div class="blog-item-inner shadow-dark">
                     <div class="blog-img">
+                      <img src={Khotta3} />
+                      <div class="blog-data">8 Sep 2021</div>
+                    </div>
+                    <div class="blog-info">
+                      <h4 class="blog-title">Khotta App | Axenda</h4>
+                      <p class="blog-descrip">Web Designer</p>
+                      <p class="blog-tags">
+                        Tags : <a href="#">Figma </a>, <a href=""> Jira </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="blog-item padd-15">
+                  <div class="blog-item-inner shadow-dark">
+                    <div class="blog-img">
+                      <img src={Project2} />
+                      <div class="blog-data">4 Feb 2021</div>
+                    </div>
+                    <div class="blog-info">
+                      <h4 class="blog-title">
+                        Hybird Software Project Management
+                      </h4>
+                      <p class="blog-descrip">Web Development</p>
+                      <p class="blog-tags">
+                        Tags : <a href="#">Next JS </a>,
+                        <a href=""> Tailwind Css </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="blog-item padd-15">
+                  <div class="blog-item-inner shadow-dark">
+                    <div class="blog-img">
                       <img src={TenderHand} />
-                      <div class="blog-data">1 Jun 2021</div>
+                      <div class="blog-data">1 Nov 2020</div>
                     </div>
                     <div class="blog-info">
                       <h4 class="blog-title">Tender Hands App</h4>
@@ -99,17 +100,15 @@ const ProjectsPage = () => {
                 <div class="blog-item padd-15">
                   <div class="blog-item-inner shadow-dark">
                     <div class="blog-img">
-                      <img src={Project2} />
-                      <div class="blog-data">8 Sep 2021</div>
+                      <img src={Cooking3} />
+                      <div class="blog-data">12 Aug 2020</div>
                     </div>
                     <div class="blog-info">
-                      <h4 class="blog-title">
-                        Hybird Software Project Management
-                      </h4>
+                      <h4 class="blog-title">Cooking Website</h4>
                       <p class="blog-descrip">Web Development</p>
                       <p class="blog-tags">
-                        Tags : <a href="#">Next JS </a>,
-                        <a href=""> Tailwind css </a>
+                        Tags : <a href="#">React JS </a>,
+                        <a href=""> Tailwind Css </a>
                       </p>
                     </div>
                   </div>
